69e9ed51ff771ba2f44147a99bc38f4b04db0c4b
Med-Bof <mohammedboaafia@gmail.com>
2024-08-13 23:35:22 +0100

    modifying code file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,11 +4,11 @@ import sys
 if __name__ == "__main__":
     """Calculate and print the sum of command-line arguments."""
     
-    num_args = len(sys.argv)  # Number of command-line arguments including the script name
-    total_sum = 0  # Initialize the sum to 0
+    num_args = len(sys.argv)
+    total_sum = 0
 
-    for index in range(1, num_args):  # Start from 1 to skip the script name
-        total_sum += int(sys.argv[index])  # Convert argument to integer and add to sum
+    for index in range(1, num_args):
+        total_sum += int(sys.argv[index])
     
-    print(total_sum)  # Print the total sum
+    print(total_sum)
 
