09886476b59da4cec5a4192048fc717fa9745987
Med-Bof <mohammedboaafia@gmail.com>
2024-08-13 23:44:02 +0100

    modifying code file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,11 +2,8 @@
 import hidden_4
 
 if __name__ == "__main__":
-    """Print all names defined in hidden_4 module that do not start with '__'."""
-
-    module_attributes = dir(hidden_4)  # Get a list of all attributes in the hidden_4 module
-    num_attributes = len(module_attributes)  # Get the total number of attributes
-
-    for index in range(num_attributes):
-        if not module_attributes[index].startswith("__"):  # Check if the name doesn't start with '__'
-            print(module_attributes[index])  # Print the name
+    File = dir(hidden_4)
+    Length = len(File)
+    for i in range(0, Length):
+        if File[i][0:2] != "__":
+            print(File[i])
