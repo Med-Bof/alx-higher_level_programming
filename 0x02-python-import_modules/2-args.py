19dafbb834fff7c954eed1f89ccdeda7ac4ceb08
Med-Bof <mohammedboaafia@gmail.com>
2024-08-13 23:29:52 +0100

    adding file 1, 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the number of command-line arguments and list them."""
+    import sys
+
+    num_args = len(sys.argv) - 1  # Number of command-line arguments excluding the script name
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_args))
+    
+    for index in range(num_args):
+        print("{}: {}".format(index + 1, sys.argv[index + 1]))
