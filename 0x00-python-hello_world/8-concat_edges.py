c2f8ec343fd22fb43f0ece02c40ba5f03ce11a83
Med-Bof <mohammedboaafia@gmail.com>
2024-09-02 19:05:21 +0100

    adding code file num 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-Py = "object-oriented programming with Python"
-print(Py)
+str = "object-oriented programming with Python"
+print(str)
