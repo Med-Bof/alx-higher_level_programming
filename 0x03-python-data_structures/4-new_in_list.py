5bb58f200a3b4f99ceb6616a949912fdcf5d0a3f
Med-Bof <mohammedboaafia@gmail.com>
2024-09-04 22:50:06 +0100

    adding code file num 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+def new_in_list(my_list, idx, element):
+    new_List = list(my_list)
+    if (idx < 0 or idx > (len(my_list) - 1)):
+        return my_list
+
+    else:
+        new_List[idx] = element
+        return new_List
