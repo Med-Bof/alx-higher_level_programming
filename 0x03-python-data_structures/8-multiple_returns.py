8158dce44e05c0ffeb33504114bcefea289cc87f
Med-Bof <mohammedboaafia@gmail.com>
2024-09-05 17:34:52 +0100

    adding code file num 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,9 @@
 
 def multiple_returns(sentence):
     lenght = len(sentence)
-    first_Char = sentence[0]
+    if len(sentence) < 1:
+        first_Char = None
+    else:
+        first_Char = sentence[0]
 
     return lenght, first_Char
